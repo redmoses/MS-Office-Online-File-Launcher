e0a533e08e7638896991b604ed3e1bab6362b383
Musa <musa.nasrullah@iappdragon.com>
2015-04-03 02:02:18 +0600

    Connects, uploads and shares file

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Poom-Poom
-Open document files from your linux file explorer in the free Microsoft Office Online version in your browser. 
+Open document files from your linux file explorer in the free Microsoft Office Online version on your browser.
 
 ## Why?
-I know Microsoft is Evil, but no matter what you can't deny one of their best creations, and that is Microsoft Office. With the free online version we can now use it on linux as well. And who are we kidding, Open Office or Libre Office is not even close to the online free version of Microsoft Office. So this why I set out on this mission, primarily to satisfy my own needs :)
+I know Microsoft is Evil, but no matter what you can't deny one of their best creations, and that is Microsoft Office. With the free online version we can now use it on linux as well. And who are we kidding, Open Office or Libre Office are not even close to the online free version of Microsoft Office. So this why I set out on this mission, primarily to satisfy my own needs :)
 
 ## Ingredients
 * Python 2.7.8
--- poom-poom.py
+++ poom-poom.py
@@ -1,6 +1,6 @@
 import ConfigParser
 import sys
-
+import os
 from dropbox.client import DropboxOAuth2FlowNoRedirect, DropboxClient
 from dropbox import rest as dbrest
 
@@ -11,42 +11,76 @@ def load_config(config_file):
     return config
 
 
+def save_token(config):
+    config_file = open('.auth', 'w')
+    config.set("Auth", "access_token", access_token)
+    config.write(config_file)
+    config_file.close()
+
+
+def try_again():
+    answer = raw_input("Do you want to try again? (y/n): ").strip()
+    if answer == 'y':
+        connect()
+    else:
+        quit(0)
+
+
 def connect():
-    auth = load_config('.auth')
-    app_key = auth.get("Auth", "APP_KEY")
-    app_secret = auth.get("Auth", "APP_SECRET")
     global access_token, user_id
 
-    auth_flow = DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+    auth = load_config('.auth')
+    app_key = auth.get("Auth", "app_key")
+    app_secret = auth.get("Auth", "app_secret")
+    access_token = auth.get("Auth", "access_token")
 
-    authorize_url = auth_flow.start()
-    print "1. Go to: " + authorize_url
-    print "2. Click \"Allow\" (you might have to log in first)."
-    print "3. Copy the authorization code."
-    auth_code = raw_input("Enter the authorization code here: ").strip()
-    try:
-        access_token, user_id = auth_flow.finish(auth_code)
-    except dbrest.ErrorResponse, e:
-        if e.status == 400:
-            print('Error: It seems your authorization code is invalid.')
-        else:
+    if access_token == '':
+        auth_flow = DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+        authorize_url = auth_flow.start()
+        print "1. Go to: " + authorize_url
+        print "2. Click \"Allow\" (you might have to log in first)."
+        print "3. Copy the authorization code."
+        auth_code = raw_input("Enter the authorization code here: ").strip()
+        try:
+            access_token, user_id = auth_flow.finish(auth_code)
+        except dbrest.ErrorResponse, e:
+            if e.status == 400:
+                print('Error: It seems your authorization code is invalid.')
+            else:
+                print("Error: %s" % (e,))
+
+            try_again()
+
+            return
+        finally:
+            save_token(auth)
+    else:
+        try:
+            dc = DropboxClient(access_token).account_info()
+        except dbrest.ErrorResponse, e:
             print("Error: %s" % (e,))
+            access_token = ''
+            save_token(auth)
+            try_again()
 
-        try_again = raw_input("Do you want to try again? (y/n): ").strip()
-        if try_again == 'y':
-            connect()
-        else:
-            quit(0)
+            return
 
-        return
+
+def upload_file(file_path):
+    dc = DropboxClient(access_token)
+    f = open(file_path, 'rb')
+    file_name = os.path.basename(file_path)
+    response = dc.put_file(file_name, f)
+    f.close()
+
+    print dc.media(file_name)
 
 
 if __name__ == '__main__':
     connect()
-    office_file = sys.argv[1]
+    file = sys.argv[1]
+    if file != '':
+        upload_file(file)
 
-    dc = DropboxClient(access_token)
-    link = dc.share('_public')
-    print link.url
-    # office_file = sys.argv[1]
 
